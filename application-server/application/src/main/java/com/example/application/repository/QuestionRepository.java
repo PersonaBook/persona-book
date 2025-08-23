0b444f087af5fb01f10dd69075577b40686720e9
jihukimme <jihu0210@naver.com>
2025-08-23 15:24:48 +0900

    refactor: 연관관계 매핑에 따른 Repository 메소드 시그니처 변경

--- application-server/application/src/main/java/com/example/application/repository/QuestionRepository.java
+++ application-server/application/src/main/java/com/example/application/repository/QuestionRepository.java
@@ -1,10 +1,12 @@
 package com.example.application.repository;
 
+import com.example.application.entity.Book;
 import com.example.application.entity.Question;
+import com.example.application.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    Optional<Question> findTopByUserIdAndBookIdOrderByCreatedAtDesc(Long userId, Long bookId);
+    Optional<Question> findTopByUserAndBookOrderByCreatedAtDesc(User user, Book book);
 }
