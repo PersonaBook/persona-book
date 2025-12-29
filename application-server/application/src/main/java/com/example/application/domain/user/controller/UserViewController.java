a8934733d8baa061638209b4e283dadc12845857
jihukimme <jihu0210@naver.com>
2025-12-30 05:16:05 +0900

    refactor: 불필요한 파라미터 제거

--- application-server/application/src/main/java/com/example/application/domain/user/controller/UserViewController.java
+++ application-server/application/src/main/java/com/example/application/domain/user/controller/UserViewController.java
@@ -1,27 +1,14 @@
 package com.example.application.domain.user.controller;
 
-import com.example.application.domain.user.dto.response.UserProfileResponseDto;
-import com.example.application.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 
 @Controller
-@RequiredArgsConstructor // 생성자 주입 (Autowired 제거)
+@RequiredArgsConstructor
 public class UserViewController {
-
-    private final UserService userService;
-
     @GetMapping("/user/profile")
-    public String getProfilePage(
-            @RequestAttribute("userId") Long userId,
-            Model model
-    ) {
-        UserProfileResponseDto userProfile = userService.getUserProfile(userId);
-        model.addAttribute("user", userProfile);
-
+    public String getProfilePage() {
         return "page/profile";
     }
 }
