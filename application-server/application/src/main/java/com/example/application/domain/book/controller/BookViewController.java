08b9ddd1a413bfdea8f4181fe15c0e3c79638061
jihukimme <jihu0210@naver.com>
2026-01-03 03:55:43 +0900

    refactor: 변수 수정

--- application-server/application/src/main/java/com/example/application/domain/book/controller/BookViewController.java
+++ application-server/application/src/main/java/com/example/application/domain/book/controller/BookViewController.java
@@ -1,6 +1,5 @@
 package com.example.application.domain.book.controller;
 
-import com.example.application.domain.book.dto.response.BookDetailResponseDto;
 import com.example.application.domain.book.service.BookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestAttribute;
 
 @Slf4j
 @Controller
@@ -20,11 +18,9 @@ public class BookViewController {
     @GetMapping("/book/{bookId}")
     public String getBookPage(
             @PathVariable Long bookId,
-            @RequestAttribute("userId") Long userId,
             Model model
     ) {
-        BookDetailResponseDto bookDetail = bookService.getBookDetail(bookId, userId);
-        model.addAttribute("book", bookDetail);
+        model.addAttribute("bookId", bookId);
         return "page/book";
     }
 }
